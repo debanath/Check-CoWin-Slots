d262ef3c48d8da88401e0d1eb0a8382f6bf5816e
Debashish Nath <debanath26@gmail.com>
2021-06-13 21:13:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+/*  fetch(
+    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=782446&date=11-06-2021"
+  )
+    .then(res => res.json())
+    .then(data => {
+      let availablePlaces = [];
+      for (let i = 0; i < data["sessions"].length; i++) {
+        let res = data["sessions"][i];
+
+        if (res["min_age_limit"] > 18 && res["available_capacity_dose1"] > 0) {
+          availablePlaces.push(res);
+        }
+      }
+      availablePlaces.length > 0
+        ? console.log(availablePlaces)
+        : console.log("Sorry no slots available");
+    }); */
+
+let pincode = 782446;
+let date = "14-06-2021"; //format DD-MM-YYYY
+let url = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${date}`;
+
+setInterval(checkAvailability, 6000);
+
+async function checkAvailability() {
+  const res = await fetch(url);
+  //console.log(url);
+  let availablePlaces = [];
+  const data = await res.json();
+  for (let i = 0; i < data["sessions"].length; i++) {
+    let res = data["sessions"][i];
+
+    if (res["min_age_limit"] >= 18 && res["available_capacity_dose1"] > 0) {
+      availablePlaces.push(res);
+    }
+  }
+  availablePlaces.length > 0
+    ? console.log(availablePlaces)
+    : console.log("No slots available");
+}
